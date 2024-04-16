13340bb0847a83ecf5956a77be42651af116d971
sam <sam@DESKTOP-HSO68FM>
2024-04-16 13:17:35 +0530

    aab commit

---
+++ spbtstcEG/src/main/java/com/spbtSTC/SpbtstcEgApplication.java
@@ -0,0 +1,13 @@
+package com.spbtSTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtstcEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbtstcEgApplication.class, args);
+	}
+
+}
---
+++ spbtstcEG/src/test/java/com/spbtSTC/SpbtstcEgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spbtSTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpbtstcEgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
